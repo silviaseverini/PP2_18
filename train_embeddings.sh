918035475b955eb55cfa0c68f2ef35919c80fd3d
fracogno <francesco.cognolato@hotmail.com>
2018-11-23 12:58:04 +0100

    Trained word2vec (4-5 grams are too big to be uploaded)

--- train_embeddings.sh
+++ train_embeddings.sh
@@ -2,12 +2,16 @@
 
 MODELS_FOLDER="./models/"
 
-# Generate corpus first
-python train_word2vec.py -i ./uniprot_sprot.fasta
-
 for ngram_size in 1 2 3 4 5
 do
 	model_path=$MODELS_FOLDER"ngram-"$ngram_size".model"
+
+	# Generate corpus first
+	python train_word2vec.py -i ./uniprot_sprot.fasta -n $ngram_size
 	
-	python train_word2vec.py -c ./corpus.txt -o $model_path -n $ngram_size
+	# Generate model
+	python train_word2vec.py -c ./corpus.txt -o $model_path
+
+	# Delete current corpus
+	rm ./corpus.txt
 done 
